b67f3c750d237dadb4879f22cd01dd254ad0effc
Ole Christian Langfjæran <judoole@gmail.com>
2016-02-24 22:18:36 +0100

    Create a Fabric deploy file

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from __future__ import with_statement
+from fabric.api import *
+import os
+
+def up():
+    if not os.environ.get('PRE_CREATE_DB'):
+        raise "PRE_CREATE_DB must be set"
+    if not os.environ.get('GF_SECURITY_ADMIN_USER'):
+        raise "GF_SECURITY_ADMIN_USER must be set"
+    if not os.environ.get('GF_SECURITY_ADMIN_PASSWORD'):
+        raise "GF_SECURITY_ADMIN_PASSWORD must be set"
+
+    local('docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d')
